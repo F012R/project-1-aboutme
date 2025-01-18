d350637dcb8afba1a31ae63e1e63ff4f145fd589
Jang Eunsun <20211234@duksung.ac.kr>
2025-01-19 06:09:42 +0900

    ✨ Feat: 현재 시간 보여주기 기능 추가

--- src/main/java/abouteunsun/TodayServlet.java
+++ src/main/java/abouteunsun/TodayServlet.java
@@ -1,40 +1,33 @@
 package abouteunsun;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TodayServlet
- */
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+     
     public TodayServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8;");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body style='text-align:center'>");
+		out.println("<a href='index.html' style='color:#727D73; text-decoration:underline;'>메인화면</a>");
+		out.println("<h1>현재 시간</h1>");
+		out.println("<div style='background-color:#D0DDD0; width:300px; padding:20px; display:block; margin:0 auto;'>" + new Date() + "</div>");
+        out.println("</body></html>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
